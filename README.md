9dded75d2c98dad0f4b108dea06559c19f389721
deepb1ue <deepb1ue@deepb1ues-MBP.fritz.box>
2015-05-17 00:01:23 +0200

    bug fixing

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
 #README
 
-##Overview
+##OVERVIEW
 
 This script scans the given network for hosts that responds to an ICMP echo.
 
-##Usage
+##USAGE
 
-``-t [TARGET NETWORK]     The target network e.g. 192.168.0.0/24
-``-
-``-h                      Displays the helpmessage
+  -n, --network [NETWORK]    Specifies the network network to be scanned. E.g. 192.168.10.0/24
+  -h, --help                 Shows a quick help
+
+##EXAMPLE
+
+Scanning the network 192.168.10.0/24 for alive hosts
+
+  ./lanscan.py -n 192.168.10.0/24
+
+##AUTHOR
+
+This script was written by Christoph Franke (christoph.franke@me.com) in 2015.
--- lanscan.py
+++ lanscan.py
@@ -4,25 +4,37 @@ import os
 from netaddr import *
 import argparse
 import subprocess
+import socket
 
 parser = argparse.ArgumentParser(description='LAN-SCAN')
-parser.add_argument('-t','--target', help='e.g. 192.168.10.0/24', required=True)
+parser.add_argument('-n','--network', help='e.g. 192.168.10.0/24', required=True)
+
 args = vars(parser.parse_args())
 
-target = str(args['target'])
+network = str(args['network'])
 
-#print ("Scanning "+target+" for hosts...)
-
-for ip in IPSet([target]):
+print ("\nScanning "+network+" for hosts...\n")
+count = 0
+hostssalive =""
+for ip in IPSet([network]):
     try:
         response = subprocess.check_output(
-        ["ping", "-c", "1", "-n", "-W", "1",  str(ip)],
-        stderr=subprocess.STDOUT,  # get all output
-        universal_newlines=True  # return string not bytes
-    )
+        ["ping", "-c", "1", "-n", "-W", "1", "-i","0.1", str(ip)],
+        stderr=subprocess.STDOUT,
+        universal_newlines=True)
         print (str(ip)+"\tis alive")
+
+        count += 1
+        hostssalive += str(ip)+"\n"
 
     except subprocess.CalledProcessError:
         response = None
 
         print (str(ip))
+
+if (int(count) == 1):
+    print ("\nSummary\n--------\n"+str(count)+" host alive")
+else:
+    print ("\nSummary:\n--------\n"+str(count)+" hosts alive")
+
+print ("\n"+hostssalive)
