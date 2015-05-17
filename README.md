14e5c681c536bac859aff0b329fc975710264196
deepb1ue <deepb1ue@deepb1ues-MBP.fritz.box>
2015-05-17 19:25:52 +0200

    bug fixing

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#README
+#LANSCAN
 
 ##OVERVIEW
 
@@ -6,15 +6,38 @@ This script scans the given network for hosts that responds to an ICMP echo.
 
 ##USAGE
 
-  -n, --network [NETWORK]    Specifies the network network to be scanned. E.g. 192.168.10.0/24
-  -h, --help                 Shows a quick help
+    -n, --network [NETWORK]
+    Specifies the network network to be scanned. E.g. 192.168.10.0/24.
+
+    -d, --dns [TRUE or FALSE]
+    Enables oder disables the name-server-lookup. The default-value is True.
+
+    -h, --help
+    Shows a quick help
 
 ##EXAMPLE
 
-Scanning the network 192.168.10.0/24 for alive hosts
+Scanning the network 10.20.0.0/30 for alive hosts:
 
-  ./lanscan.py -n 192.168.10.0/24
+    ./lanscan.py -n 10.20.0.0/30
 
-##AUTHOR
+Example output:
 
-This script was written by Christoph Franke (christoph.franke@me.com) in 2015.
+    Scanning 10.20.0.0/30 for hosts...
+
+    IP          Status  Name
+    --		    ------  -----
+    10.20.0.0
+    10.20.0.1   ALIVE   rtr001
+    10.20.0.2   ALIVE   rtr002
+    10.20.0.3
+
+    Summary:
+    --------
+    2 hosts alive
+    10.20.0.1 (rtr001)
+    10.20.0.2 (rtr002)
+
+##CONTACT
+
+You can contact me via mail: [mail@sysadmin-log.de](mailto:mail@sysadmin-log.de).
--- lanscan.py
+++ lanscan.py
@@ -6,14 +6,37 @@ import argparse
 import subprocess
 import socket
 
+# DNS Lookup
+def lookup(addr):
+    try:
+        return socket.gethostbyaddr(str(addr))
+    except:
+        return None, None, None
+
+# Pasring the Arguments
 parser = argparse.ArgumentParser(description='LAN-SCAN')
 parser.add_argument('-n','--network', help='e.g. 192.168.10.0/24', required=True)
+parser.add_argument('-d','--dns', help='Set DNS lookup True or False')
 
 args = vars(parser.parse_args())
-
 network = str(args['network'])
+dns = str(args['dns'])
 
+# Check if DNS Lookup-Argument is set
+if(dns==None):
+    nslookup=True
+elif(dns=="True"):
+    nslookup=True
+elif(dns=="False"):
+    nslookup=False
+else:
+    nslookup=True
+
+# Check-Loop
 print ("\nScanning "+network+" for hosts...\n")
+
+print ("IP\t\tStatus\tName")
+print ("--\t\t------\t-----")
 count = 0
 hostssalive =""
 for ip in IPSet([network]):
@@ -22,19 +45,33 @@ for ip in IPSet([network]):
         ["ping", "-c", "1", "-n", "-W", "1", "-i","0.1", str(ip)],
         stderr=subprocess.STDOUT,
         universal_newlines=True)
-        print (str(ip)+"\tis alive")
+
+        if (nslookup):
+            name,alias,addresslist = lookup(ip)
+            if (name==None):
+                name = ""
+        else:
+            name = ""
+        print (str(ip)+"\tALIVE\t"+name)
 
         count += 1
-        hostssalive += str(ip)+"\n"
+        if (name != ""):
+            hostssalive += str(ip)+" ("+name+")"+"\n"
+        else:
+            hostssalive += str(ip)+"\n"
 
     except subprocess.CalledProcessError:
         response = None
 
         print (str(ip))
 
+# Summary 
 if (int(count) == 1):
     print ("\nSummary\n--------\n"+str(count)+" host alive")
 else:
     print ("\nSummary:\n--------\n"+str(count)+" hosts alive")
 
-print ("\n"+hostssalive)
+if (hostssalive != ""):
+    print ("\n"+hostssalive)
+else:
+    print ("")
