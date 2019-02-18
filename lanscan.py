6f18fe6159ff3516facea3519fd2d0eafa7898f1
Chris <christoph.franke@outlook.com>
2019-02-18 09:29:15 +0100

    Update lanscan.py

--- lanscan.py
+++ lanscan.py
@@ -1,132 +1,108 @@
 #!/usr/bin/python
 
-import os
+# Wolters Kluwer Deutschland
+# Christoph Franke
+# christoph.franke@wolterskluwer.com
+# 12.02.2019
+
 from netaddr import *
-import argparse
-import subprocess
 import socket
-from sys import argv
-import datetime
-from prettytable import PrettyTable
-from progressbar import Bar, Percentage, ProgressBar, ETA, SimpleProgress
-import progressbar
-# DNS Lookup
+import subprocess
+import argparse
+import signal
+import sys
+
+# Catch SIGIN STRG+C
+def signal_handler(sig, frame):
+        print('Scan stopped.')
+        sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
+
+# Portscan
+def checkPort(ip, port):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(0.3)
+        result = s.connect((ip, port))
+        s.shutdown(1)
+        return True
+    except:
+        return False
+
+# Reverse Lookup
 def lookup(addr):
     try:
-        return socket.gethostbyaddr(str(addr))
+        data = socket.gethostbyaddr(str(addr))
+        host = repr(data[0])
+        host = str(host)
+        host = host.strip("'")
+        return host
     except:
-        return None, None, None
+        return "NA"
+
+# Color Output
+col_green =  "\033[0;32m"
+col_red = "\033[1;31m"
+col_norm = "\033[0m"
 
 # Pasring the Arguments
-parser = argparse.ArgumentParser(description='LAN-SCAN')
+parser = argparse.ArgumentParser(description='Subnet Scanner')
 parser.add_argument('-n','--network', help='e.g. 192.168.10.0/24', required=True)
-parser.add_argument('-d','--dns', help='FALSE sets DNS lookup active or inactive')
-parser.add_argument('-l','--log', help='TRUE creates a comma-seperated-logfile')
-parser.add_argument('-o','--online', default='True', help='show only online hosts')
-
+parser.add_argument('-t','--timeout', help='timeout in seconds', default='0.2')
 args = vars(parser.parse_args())
 network = str(args['network'])
-dns = str(args['dns'])
-online = str(args['online'])
+timeout = str(args['timeout'])
 
-log = str(args['log'])
-if (log==None):
-    logging=False
-elif ((log=="True") or (log=="TRUE") or (log=="true")):
-    logging=True
-elif ((log=="False") or (log=="FALSE") or (log=="false")):
-    logging=False
-else:
-    logging=False
+ftp = "0"
+ssh = "0"
+http = "0"
+https = "0"
+smb = "0"
+rdp = "0"
 
-# Check if DNS Lookup-Argument is set
-if(dns==None):
-    nslookup=True
-elif((dns=="True") or (dns=="true") or (dns=="TRUE")):
-    nslookup=True
-elif((dns=="False") or (dns=="FALSE") or (dns=="false")):
-    nslookup=False
-else:
-    nslookup=True
+# Print Header
+print ("IP, STATUS, HOSTNAME, ftp, ssh, http, https, smb, rdp")
 
-# Check-Loop
-print ("\n** Scanning "+network+" for hosts **\n")
-
-#print ("IP\t\tStatus\tName")
-#print ("--\t\t--\t--")
-x = PrettyTable(["IP Address", "Status", "Name"])
-x.align["IP Address"] = "l" 
-x.align["Name"] ="l"
-x.padding_width = 1
-count = 0
-hostssalive =""
-ips = 0
-
-#logging
-ts= str(datetime.date.today())
-
-if (logging==True):
-    fw = open('ipscan_'+ts+'.csv','w')
-    fw.write("IP,Status,Name\n")
-
-pbar = ProgressBar(widgets=[Percentage(), Bar(), ETA()], maxval=len(IPSet([network]))).start()
-
-for ip in  IPSet([network]):
+# Loop through Subnet and try to ping and portscan host
+for ip in IPSet([network]):
     try:
         response = subprocess.check_output(
-        ["ping", "-c", "1", "-n", "-W", "1", "-i","0.1", str(ip)],
+        ['ping', '-c', '3', "-W", "1", "-i",timeout, str(ip)],
         stderr=subprocess.STDOUT,
         universal_newlines=True)
-
-        if (nslookup):
-            name,alias,addresslist = lookup(ip)
-            if (name==None):
-                name = ""
+        online = True
+        icmp = True
+        hostname = str(lookup (str(ip)))
+        if (checkPort(str(ip), 21)):
+            online = True
+            ftp = "1"
+        if (checkPort(str(ip), 22)):
+            online = True
+            ssh = "1"
+        if (checkPort(str(ip), 80)):
+            online = True
+            http = "1"
+        if (checkPort(str(ip), 443)):
+            online = True
+            https = "1"
+        if (checkPort(str(ip), 445)):
+            online = True
+            smb = "1"
+        if (checkPort(str(ip), 3389)):
+            online = True
+            rdp = "1"
+        if (online == True):
+            print (str(ip)+ ", " +col_green +"ONLINE"+col_norm+", "+hostname+", "+ftp+", "+ssh+", "+http+", "+https+", "+smb+", "+rdp)
         else:
-            name = ""
-        #print (str(ip)+"\tALIVE\t"+name)
-        x.add_row([str(ip),"ONLINE",name])
-        if (logging==True):
-            fw.write(str(ip)+",ALIVE,"+name+"\n")
+            print (str(ip)+", "+col_red+"OFFLINE"+col_norm+", NA")
 
-        count += 1
-        if (name != ""):
-            hostssalive += str(ip)+"\t"+name+"\n"
-        else:
-            hostssalive += str(ip)+"\n"
+        ftp = "0"
+        ssh = "0"
+        http = "0"
+        https = "0"
+        smb = "0"
+        rdp = "0"
 
+# Catch Error
     except subprocess.CalledProcessError:
-        response = None
-        if (online!="True"):
-           # print (str(ip))
-            x.add_row([str(ip),"OFFLINE",""])
-            if (logging==True):
-                fw.write((str(ip)+"\n"))
-   # r.update(ips)
-    pbar.update(ips)
-    ips=ips+1
-pbar.finish()
-print ("\n")
-print x
-
-
-print ("\n")# Summary
-#print ("\nSummary\n--\n"+str(ips)+" IP's scanned")
-y = PrettyTable(["IPs scanned", "IPs alive"])
-y.align["IPs scanned"] = "l" # Left align city names
-y.align["IPs alive"] = "l" # Left align city names
-y.padding_width = 1
-
-y.add_row([str(ips), str(count)])
-print y
-#
-#if (int(count) == 1):
-#    print (str(count)+" host alive")
-#else:
-#    print (str(count)+" hosts alive")
-#if (online!="True"):
-#    if (hostssalive != ""):
-#        print ("\n"+hostssalive)
-#    else:
-#        print ("")
-print ("\n")
+        print (str(ip)+", "+col_red+"OFFLINE"+col_norm+", NA")
